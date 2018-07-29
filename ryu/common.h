e058a73b35afaca5c840e34e52295dda4c5d276d
Ulf Adams <ulfjack@users.noreply.github.com>
2018-07-29 10:28:02 +0200

    Update common.h
    Fix typo, edit a bit for brevity.

--- ryu/common.h
+++ ryu/common.h
@@ -22,9 +22,9 @@
 
 // Returns e == 0 ? 1 : ceil(log_2(5^e)).
 static inline uint32_t pow5bits(const int32_t e) {
-  // This approximation works up to the point that the multiplcation overflows at e = 3529.
-  // If the multiplication were done in 64 bits the first value it would fail at
-  // is 5^4004 which is just greater than 2^9297.
+  // This approximation works up to the point that the multiplication overflows at e = 3529.
+  // If the multiplication were done in 64 bits, it would fail at 5^4004 which is just greater
+  // than 2^9297.
   assert(e >= 0);
   assert(e <= 3528);
   return ((((uint32_t) e) * 1217359) >> 19) + 1;
