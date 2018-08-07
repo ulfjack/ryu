1995dd9a734ed6a0a1ae16f62be59a0d56718833
Ulf Adams <ulfjack@google.com>
2018-08-07 15:12:00 +0200

    Add long double (80-bit float) interface and basic tests

--- ryu/ryu_generic_128.h
+++ ryu/ryu_generic_128.h
@@ -35,11 +35,12 @@ struct floating_decimal_128 {
 
 struct floating_decimal_128 float_to_fd128(float f);
 struct floating_decimal_128 double_to_fd128(double d);
+struct floating_decimal_128 long_double_to_fd128(long double d);
 
 // Converts the given binary floating point number to the shortest decimal floating point number
 // that still accurately represents it.
 struct floating_decimal_128 generic_binary_to_decimal(
-    const __uint128_t bits, const uint32_t mantissaBits, const uint32_t exponentBits);
+    const __uint128_t bits, const uint32_t mantissaBits, const uint32_t exponentBits, const bool explicitLeadingBit);
 
 // Converts the given decimal floating point number to a string, writing to result, and returning
 // the number characters written. Does not terminate the buffer with a 0. In the worst case, this
