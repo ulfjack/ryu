1080740db2a5cd0c239f76d6a1b7b010403fed36
Ulf Adams <ulfjack@users.noreply.github.com>
2018-08-16 14:28:04 +0200

    Replace decimalLength
    Remove the old decimalLength implementation in favor of the new one.

--- ryu/d2s.c
+++ ryu/d2s.c
@@ -206,31 +206,6 @@ static inline uint64_t mulShiftAll(
 #endif // HAS_64_BIT_INTRINSICS
 
 static inline uint32_t decimalLength(const uint64_t v) {
-  // This is slightly faster than a loop.
-  // The average output length is 16.38 digits, so we check high-to-low.
-  // Function precondition: v is not an 18, 19, or 20-digit number.
-  // (17 digits are sufficient for round-tripping.)
-  assert(v < 100000000000000000L);
-  if (v >= 10000000000000000L) { return 17; }
-  if (v >= 1000000000000000L) { return 16; }
-  if (v >= 100000000000000L) { return 15; }
-  if (v >= 10000000000000L) { return 14; }
-  if (v >= 1000000000000L) { return 13; }
-  if (v >= 100000000000L) { return 12; }
-  if (v >= 10000000000L) { return 11; }
-  if (v >= 1000000000L) { return 10; }
-  if (v >= 100000000L) { return 9; }
-  if (v >= 10000000L) { return 8; }
-  if (v >= 1000000L) { return 7; }
-  if (v >= 100000L) { return 6; }
-  if (v >= 10000L) { return 5; }
-  if (v >= 1000L) { return 4; }
-  if (v >= 100L) { return 3; }
-  if (v >= 10L) { return 2; }
-  return 1;
-}
-
-static inline uint32_t decimalLength2(const uint64_t v) {
   static const uint64_t table[18] = {
     0ull,
     9ull,
