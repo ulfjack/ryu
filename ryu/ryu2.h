44f7a73252922c6b5df721504857919582e7886e
Ulf Adams <ulfjack@google.com>
2018-09-22 00:51:30 +0200

    Initial attempt at generating full output

---
+++ ryu/ryu2.h
@@ -0,0 +1,32 @@
+// Copyright 2018 Ulf Adams
+//
+// The contents of this file may be used under the terms of the Apache License,
+// Version 2.0.
+//
+//    (See accompanying file LICENSE-Apache or copy at
+//     http://www.apache.org/licenses/LICENSE-2.0)
+//
+// Alternatively, the contents of this file may be used under the terms of
+// the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE-Boost or copy at
+//     https://www.boost.org/LICENSE_1_0.txt)
+//
+// Unless required by applicable law or agreed to in writing, this software
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.
+#ifndef RYU2_H
+#define RYU2_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int d2fixed_buffered_n(double d, char* result);
+//void d2fixed_buffered(double d, char* result);
+char* d2fixed(double d);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // RYU2_H
