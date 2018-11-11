a23861d6fb69e81eb5770b6f092e7998457b9d33
Ulf Adams <ulfjack@google.com>
2018-11-11 07:26:54 -0500

    Temporary benchmark script for Windows

---
+++ run_benchmark.bat
@@ -0,0 +1,9 @@
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -samples=1000 -v -precision=1 > win-fixed-1.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -samples=1000 -v -precision=10 > win-fixed-10.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -samples=1000 -v -precision=100 > win-fixed-100.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -samples=1000 -v -precision=1000 > win-fixed-1000.csv
+
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -exp -samples=1000 -v -precision=1 > win-exp-1.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -exp -samples=1000 -v -precision=10 > win-exp-10.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -exp -samples=1000 -v -precision=100 > win-exp-100.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -exp -samples=1000 -v -precision=1000 > win-exp-1000.csv
