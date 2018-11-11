91a6423a48a6af26ae68aaf8652e3ad20e708ae1
Ulf Adams <ulfjack@google.com>
2018-11-11 07:39:03 -0500

    Attempted fix ugh

--- run_benchmark.bat
+++ run_benchmark.bat
@@ -1,7 +1,7 @@
-bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -samples=1000 -v -precision=1 > win-fixed-1.csv
-bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -samples=1000 -v -precision=10 > win-fixed-10.csv
-bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -samples=1000 -v -precision=100 > win-fixed-100.csv
-bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -samples=1000 -v -precision=1000 > win-fixed-1000.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -64 -samples=1000 -v -precision=1 > win-fixed-1.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -64 -samples=1000 -v -precision=10 > win-fixed-10.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -64 -samples=1000 -v -precision=100 > win-fixed-100.csv
+bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -64 -samples=1000 -v -precision=1000 > win-fixed-1000.csv
 
 bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -exp -samples=1000 -v -precision=1 > win-exp-1.csv
 bazel run -c opt //ryu/benchmark:benchmark_fixed_cc -- -exp -samples=1000 -v -precision=10 > win-exp-10.csv
