0854bfe6cb788fde9a4bf0f79831e9c963addfe0
Ulf Adams <ulfjack@gmail.com>
2018-11-17 12:58:11 +0100

    Fix

--- run_benchmark_linux.sh
+++ run_benchmark_linux.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#for i in 1 10 100 1000; do
-#  echo $i
-#  CC=gcc bazel run -c opt //ryu/benchmark:benchmark_fixed -- -samples=1000 -v -precision=$i > glibc-fixed-$i.csv
-#  CC=gcc bazel run -c opt //ryu/benchmark:benchmark_fixed -- -samples=1000 -v -precision=$i -exp > glibc-exp-$i.csv
-#done
-
 for i in 1 10 100 1000; do
   echo $i
-  CC=musl-gcc bazel run -c opt //ryu/benchmark:benchmark_fixed -- -samples=1000 -v -precision=$i > musl-fixed-$i.csv
-  CC=musl-gcc bazel run -c opt //ryu/benchmark:benchmark_fixed -- -samples=1000 -v -precision=$i -exp > musl-exp-$i.csv
+  CC=clang-6.0 bazel run -c opt //ryu/benchmark:benchmark_fixed -- -samples=1000 -v -precision=$i > glibc-fixed-$i.csv
+  CC=clang-6.0 bazel run -c opt //ryu/benchmark:benchmark_fixed -- -samples=1000 -v -precision=$i -exp > glibc-exp-$i.csv
 done
+
+#for i in 1 10 100 1000; do
+#  echo $i
+#  CC=musl-gcc bazel run -c opt //ryu/benchmark:benchmark_fixed -- -samples=1000 -v -precision=$i > musl-fixed-$i.csv
+#  CC=musl-gcc bazel run -c opt //ryu/benchmark:benchmark_fixed -- -samples=1000 -v -precision=$i -exp > musl-exp-$i.csv
+#done
 
