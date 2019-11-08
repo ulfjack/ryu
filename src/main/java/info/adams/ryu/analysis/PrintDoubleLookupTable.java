fc1525792dcb8b50759acb08fcbc51bb1c5c4f88
Ulf Adams <ulfjack@gmail.com>
2019-11-08 16:25:45 +0100

    Use larger bit counts
    This is in preparation for reusing the double tables for float conversion. By making them exactly 64 bit larger than required for float conversion, we can simply take the upper 64 bit of the table entry, which is exactly the same value as the float table.

--- src/main/java/info/adams/ryu/analysis/PrintDoubleLookupTable.java
+++ src/main/java/info/adams/ryu/analysis/PrintDoubleLookupTable.java
@@ -24,10 +24,11 @@ public final class PrintDoubleLookupTable {
   // The C version has two code paths, one of which requires an additional entry here.
   private static final int NEG_TABLE_SIZE = 291 + 1;
 
-  private static final int POW5_BITCOUNT = 121; // max 127
-  private static final int POW5_INV_BITCOUNT = 122; // max 127
+  // We intentionally choose these to be larger than or equal to the float equivalent + 64.
+  private static final int POW5_BITCOUNT = 125; // max 127
+  private static final int POW5_INV_BITCOUNT = 125; // max 127
 
-  private static final boolean PRINT_LARGE_TABLES = true;
+  private static final boolean PRINT_LARGE_TABLES = false;
 
   private static final BigInteger MASK64 = BigInteger.valueOf(1).shiftLeft(64).subtract(BigInteger.ONE);
 
