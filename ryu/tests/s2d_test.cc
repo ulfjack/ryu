d4114cedf9de3a9504e0fc21e212ef1043af64c7
Ulf Adams <ulfjack@users.noreply.github.com>
2019-12-04 15:28:49 +0100

    Ryu-like String to Float Parsing (#138)
    A limited implementation for up to 17-digit numbers.
    
    See #129.
    
    * Move some d2s intrinsics to d2s_intrinsics.h
    
    * Rudimentary string to double parsing
    
    * Implement more pieces for parsing
    
    - parse exponents
    - parse dot
    - handle negative exponents
    - handle rounding
    - add some basic tests for these things
    
    * Fix constants
    
    * Use __builtin_clzll
    
    Thanks to expnkx for finding this.
    
    See #133.
    
    * Allow exponent to start with '+'
    
    * Correctly handle mantissa overflow
    
    * Increase the table size
    
    Denormal numbers require a larger table because the decimal exponent can go down to -326 (we're using the table inversely to how it's originally used for shortest).
    
    * Handle most overflow/underflow situations
    
    * Increase table size some more
    
    * More tests close to the underflow
    
    These are almost exactly halfway between 0 and the smallest representable float.
    
    * Implement error handling
    
    Mention the restrictions on the input, and that this implementation is experimental.
    
    * Add another caveat to the documentation
    
    * Make it compile with -DRYU_ONLY_64_BIT_OPS
    
    This mode was not defining mulShift. We just use the same implementation as if it's not set.
    
    * Use _BitScanReverse64 on Windows
    
    * Rename mulShift to mulShift{32,64}
    
    Since I moved the mulShift implementations to d2s_intrinsics, they conflict with the ones defined in f2s.c. This is only an issue when -DRYU_OPTIMIZE_SIZE is set, but I think it's better to disambiguate anyway.
    
    * Rename max to max32 to avoid name conflicts on MSVC
    
    * MSVC: Always use _BitScanReverse64
    
    The HAS_64_BIT_INTRINSICS macro is false if RYU_ONLY_64_BIT_OPS is set, so using that is incorrect. Instead, only check for _MSC_VER. I can't seem to find a macro to check for existence of __builtin_clzll.

---
+++ ryu/tests/s2d_test.cc
@@ -0,0 +1,87 @@
+// Copyright 2019 Ulf Adams
+//
+// The contents of this file may be used under the terms of the Apache License,
+// Version 2.0.
+//
+//    (See accompanying file LICENSE-Apache or copy at
+//     http://www.apache.org/licenses/LICENSE-2.0)
+//
+// Alternatively, the contents of this file may be used under the terms of
+// the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE-Boost or copy at
+//     https://www.boost.org/LICENSE_1_0.txt)
+//
+// Unless required by applicable law or agreed to in writing, this software
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.
+
+#include <math.h>
+
+#include "ryu/ryu_parse.h"
+#include "third_party/gtest/gtest.h"
+
+#define EXPECT_S2D(a, b) { double value; EXPECT_EQ(SUCCESS, s2d(b, &value)); EXPECT_EQ(a, value); } while (0);
+
+TEST(S2dTest, BadInput) {
+  double value;
+  EXPECT_EQ(MALFORMED_INPUT, s2d("x", &value));
+  EXPECT_EQ(MALFORMED_INPUT, s2d("1..1", &value));
+  EXPECT_EQ(MALFORMED_INPUT, s2d("..", &value));
+  EXPECT_EQ(MALFORMED_INPUT, s2d("1..1", &value));
+  EXPECT_EQ(MALFORMED_INPUT, s2d("1ee1", &value));
+  EXPECT_EQ(MALFORMED_INPUT, s2d("1e.1", &value));
+  EXPECT_EQ(INPUT_TOO_SHORT, s2d("", &value));
+  EXPECT_EQ(INPUT_TOO_LONG, s2d("123456789012345678", &value));
+  EXPECT_EQ(INPUT_TOO_LONG, s2d("1e12345", &value));
+}
+
+TEST(S2dTest, Basic) {
+  EXPECT_S2D(0.0, "0");
+  EXPECT_S2D(-0.0, "-0");
+  EXPECT_S2D(1.0, "1");
+  EXPECT_S2D(2.0, "2");
+  EXPECT_S2D(123456789.0, "123456789");
+  EXPECT_S2D(123.456, "123.456");
+  EXPECT_S2D(123.456, "123456e-3");
+  EXPECT_S2D(123.456, "1234.56e-1");
+  EXPECT_S2D(1.453, "1.453");
+  EXPECT_S2D(1453.0, "1.453e+3");
+  EXPECT_S2D(0.0, ".0");
+  EXPECT_S2D(1.0, "1e0");
+  EXPECT_S2D(1.0, "1E0");
+  EXPECT_S2D(1.0, "000001.000000");
+}
+
+TEST(S2dTest, MinMax) {
+  EXPECT_S2D(1.7976931348623157e308, "1.7976931348623157e308");
+  EXPECT_S2D(5E-324, "5E-324");
+}
+
+TEST(S2dTest, MantissaRoundingOverflow) {
+  // This results in binary mantissa that is all ones and requires rounding up
+  // because it is closer to 1 than to the next smaller float. This is a
+  // regression test that the mantissa overflow is handled correctly by
+  // increasing the exponent.
+  EXPECT_S2D(1.0, "0.99999999999999999");
+  // This number overflows the mantissa *and* the IEEE exponent.
+  EXPECT_S2D(INFINITY, "1.7976931348623159e308");
+}
+
+TEST(S2dTest, Underflow) {
+  EXPECT_S2D(0.0, "2.4e-324");
+  EXPECT_S2D(0.0, "1e-324");
+  EXPECT_S2D(0.0, "9.99999e-325");
+  // These are just about halfway between 0 and the smallest float.
+  // The first is just below the halfway point, the second just above.
+  EXPECT_S2D(0.0, "2.4703282292062327e-324");
+  EXPECT_S2D(5e-324, "2.4703282292062328e-324");
+}
+
+TEST(S2dTest, Overflow) {
+  EXPECT_S2D(INFINITY, "2e308");
+  EXPECT_S2D(INFINITY, "1e309");
+}
+
+TEST(S2dTest, TableSizeDenormal) {
+  EXPECT_S2D(5e-324, "4.9406564584124654e-324");
+}
